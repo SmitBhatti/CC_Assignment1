2d76d30f611bc476ddaac46a715853d233835711
SmitBhatti <s3802327@student.rmit.edu.au>
2022-04-28 04:02:13 +1000

    Added requirements file

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from flask_share import Share
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
