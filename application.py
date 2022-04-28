0297f1f251c54dff0f621233d67462e01ed955f2
SmitBhatti <s3802327@student.rmit.edu.au>
2022-04-28 16:17:18 +1000

    Added configurations

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from website import create_app
 
+application = create_app()
 if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
+    application.run(debug=True)
--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,7 @@ DB_NAME = "database.db"
 share = Share()
 
 def create_app():
-    app = Flask(__name__)
+    application = app = Flask(__name__)
     app.config['SECRET_KEY'] = "RMIT"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
